ce3cf75a923c2fc699d135686daae28de4ee0622
Berke <tormorkor@outlook.com>
2023-03-19 17:08:49 +0300

    feat: assets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use rusty_engine::prelude::*;
+
+struct GameState {
+    health_left: i32,
+}
+
+
 fn main() {
+    let mut game = Game::new();
     println!("Hello, world!");
+    game.run(GameState { health_left: 42 });
 }
